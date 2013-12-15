36b8d7e2ecbbd29ecfc880714bbe5ea3faa4a067
Kris Erickson <ksoncan34@yahoo.ca>
2013-12-14 17:16:21 -0800

    Got the gallery working.  The carousel is there but it doesn't work yet...

--- js/app.js
+++ js/app.js
@@ -2,26 +2,54 @@
  * Demo App for TopcoatTouch
  */
 $(document).ready(function() {
-    var topcoatTouch = new TopcoatTouch($('body'));
-    topcoatTouch.goTo('home');
 
+    // Create the topcoatTouch object
+    var tt = new TopcoatTouch($('body'));
+    var carouselScroll = null;
+
+    // First page we go to home...  This could be done in code by setting the class to 'page page-center', but here is how to do it in code...
+    tt.goTo('home');
+
+    //
     $('#showLoading').click(function() {
-        topcoatTouch.showLoading('10 seconds');
+        tt.showLoading('10 seconds');
         var count = 10;
         var interval = setInterval(function() {
             if (--count <= 0) {
                 clearInterval(interval);
-                topcoatTouch.hideLoading();
+                tt.hideLoading();
             } else {
                 $('#topcoat-loading-message').text(count + ' seconds');
             }
         },1000);
     });
+
+    // Show the dialog...
     $('#showDialog').click(function() {
-        topcoatTouch.showDialog('<h3>Example Dialog</h3><div>This is a dialog</div>', {OK: function() { console.log('OK Pressed') }
+        tt.showDialog('<h3>Example Dialog</h3><div>This is a dialog</div>', {OK: function() { console.log('OK Pressed') }
             , Cancel: function() { console.log('Cancel Pressed')}});
     });
-    $('#testDialog').find('button').click(function() {
-        topcoatTouch.hideDialog();
+
+
+    tt.on(tt.EVENTS.PAGE_START, 'carouselExample', function() {
+        carouselScroll = new IScroll('#carouselWrapper', {
+            scrollX: true,
+            scrollY: false,
+            momentum: false,
+            snap: true,
+            snapSpeed: 400,
+            keyBindings: true,
+            indicators: {
+                el: document.getElementById('carouselIndicator'),
+                resize: false
+            }
+        });
+    }).on(tt.EVENTS.PAGE_START, 'carouselExample', function() {
+        if (carouselScroll != null) {
+            carouselScroll.destroy();
+            carouselScroll = null;
+        }
     });
+
+
 });
