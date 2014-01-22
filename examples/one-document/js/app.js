296465421f7ca5f9564a2e4d831febcfc53d518d
Kris Erickson <ksoncan34@yahoo.ca>
2014-01-22 08:38:36 -0800

    Started menu demo

--- examples/one-document/js/app.js
+++ examples/one-document/js/app.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
 
     // Create the topcoatTouch object
-    var tt = new TopcoatTouch();
+    var tt = new TopcoatTouch({menu: [{id: 'help', name: 'Help'}, {id: 'about', name: 'About'}]});
     // First page we go to home...  This could be done in code by setting the class to 'page page-center', but here is how to do it in code...
     tt.goTo('home');
 
